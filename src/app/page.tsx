9b37821c6a29535b1abc80fb2e69968b850b686a
Connor Davis <cnnrproton@gmail.com>
2023-10-11 16:06:13 +0200

    Themes update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
+import ThemeSelector from "@/components/theme-selecter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
 import { Menubar } from "@/components/ui/menubar";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen py-2 px-96">
-      <Menubar className="p-5">
-        <Label className="text-2xl font-bold text-primary">
-          <Link href="/">Autobot</Link>
-        </Label>
-      </Menubar>
+    <div className="flex flex-col w-screen h-screen">
+      <div className="flex items-center justify-between p-3 border-b border-border">
+        <div className="flex items-center space-x-5">
+          <Label className="text-2xl font-bold text-primary">
+            <Link href="/">Autobot</Link>
+          </Label>
+        </div>
+
+        <div className="flex items-center space-x-5">
+          <ThemeSelector />
+
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
     </div>
   );
 }
